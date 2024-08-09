b1e2e9d9198863b40f0321220c7c1293a1c4a987
MISSIC17 <prajjwalt9@gmail.com>
2024-08-09 17:34:18 +0545

    feat: setup config, shadcn,font

---
+++ src/config/site-config.ts
@@ -0,0 +1,14 @@
+/**
+ * Site config containing all the site related information
+ */
+
+export const siteConfig = {
+  siteName: "Himalayan Adventure",
+  siteUrl: "",
+  siteDescription: "",
+
+  publisher: "Himalayan Adventure",
+
+  supportEmail: "xxx@nxxx.com",
+  supportPhone: "+977-123-456-7890",
+};
