ac4c80d9dc87749a314c1a140e349113d2416073
MISSIC17 <prajjwalt9@gmail.com>
2024-08-14 17:51:39 +0545

    fix: image responsivenss

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,7 +6,7 @@ import climberImage from "/public/images/climber-bg.png";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="absolute left-0 top-0 h-[50vh] w-full overflow-hidden md:h-screen">
+      <div className="absolute left-0 top-0 h-[50vh] w-full overflow-hidden sm:h-screen">
         <Image
           src={bgImage}
           alt="Home horizon image"
@@ -15,17 +15,17 @@ export default function Home() {
         <Image
           src={climberImage}
           alt="Climber Image"
-          className="absolute bottom-10 left-0 h-[90vh] w-auto object-contain mix-blend-color-dodge"
+          className="absolute bottom-[15%] left-0 h-1/2 w-auto object-contain mix-blend-color-dodge sm:bottom-10 sm:h-[90vh]"
         />
         <Image
           src={lhotseImage}
           alt="Lhoste Image"
-          className="absolute bottom-20 right-0 w-auto object-cover md:bottom-0 md:h-screen md:w-full"
+          className="absolute bottom-10 right-0 h-full w-auto object-cover md:bottom-0 md:h-screen md:w-full"
         />
         <Image
           src={cloudImage}
           alt="Cloud Image"
-          className="absolute bottom-10 left-0 h-1/2 object-cover md:-bottom-10 md:h-[60vh]"
+          className="absolute bottom-0 left-0 h-1/2 object-cover md:-bottom-10 md:h-[60vh]"
         />
         Himalayan Adventures Home Page
       </div>
