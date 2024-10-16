7e543d3fc570ad21cf27f30a27ab6820acd1117c
Binita Shrestha <binitastha34@gmail.com>
2024-10-16 18:52:51 +0545

    feat: about page ui complete

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import logo from "/public/logo.svg";
 import logoLight from "/public/logo-light.svg";
+import logoBlack from "/public/logo-black.svg";
 
 export default function Logo({
   variant = "large",
@@ -19,6 +20,28 @@ export default function Logo({
       <p className="sr-only">Himalayan Adventures logo</p>
       <Image
         src={theme === "dark" ? logo : logoLight}
+        alt="logo"
+        className={cn(className)}
+        priority
+      />
+    </Link>
+  );
+}
+
+export function BlackLogo({
+  variant = "large",
+  className,
+  theme = "dark",
+}: {
+  variant?: "large" | "small";
+  className?: string;
+  theme?: "light" | "dark";
+}): JSX.Element {
+  return (
+    <Link href={"/"} className="block w-fit">
+      <p className="sr-only">Himalayan Adventures logo</p>
+      <Image
+        src={theme === "light" ? logo : logoBlack}
         alt="logo"
         className={cn(className)}
         priority
