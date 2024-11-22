edcfe69f11480363680e164a69de652b13eae165
Binita Shrestha <binitastha34@gmail.com>
2024-11-22 11:18:05 +0545

    feat: added  three buttons on departure card

--- src/components/packagespage/departure.tsx
+++ src/components/packagespage/departure.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { LuStar } from "react-icons/lu";
 import { Button } from "../ui/button";
 import { MdTimelapse } from "react-icons/md";
@@ -8,10 +8,17 @@ import { FaMountain } from "react-icons/fa";
 import Link from "next/link";
 import { seasonIconMap, seasonMonthMap } from "@/config/ui-constants";
 import { formatDate } from "@/lib/utils";
-import { Text } from "../ui/text";
 import wordsToNumbers from "words-to-numbers";
 import { DepartureProps } from "@/types/packages/departure";
 import { Flag } from "lucide-react";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
 
 export default function Departure({
   data,
@@ -22,6 +29,13 @@ export default function Departure({
   const maxAltInM = Number(
     wordsToNumbers(altitude)?.toString().split(" ").join(""),
   );
+
+  const [selectedPackage, setSelectedPackage] = useState<string>("");
+
+  const handleButtonClick = (packageType: string) => {
+    setSelectedPackage(packageType);
+  };
+
   const departureFacts = [
     {
       title: season,
@@ -40,12 +54,13 @@ export default function Departure({
     },
     {
       title: "max altitude",
-      desc: `${maxAltInM.toLocaleString("en-us")}m/${(maxAltInM * 3.281).toLocaleString("en-us")}ft`,
+      desc: `${maxAltInM.toLocaleString(
+        "en-us",
+      )}m/${(maxAltInM * 3.281).toLocaleString("en-us")}ft`,
       icon: <FaMountain size={20} />,
     },
   ];
 
-  console.log(departure);
   return (
     <div className="relative space-y-4">
       <div className="relative rounded-lg border p-2 shadow-xl shadow-gray-300 lg:p-4">
@@ -89,7 +104,95 @@ export default function Departure({
                 ))}
               </div>
             )}
-          <button className="mt-4 w-full rounded-lg bg-primary py-2 font-semibold text-white hover:bg-orange-500">
+
+          <Dialog>
+            {/* Trigger Buttons */}
+            <div className="mt-4 flex flex-col items-center justify-center gap-4 md:mt-8">
+              <DialogTrigger asChild>
+                <button
+                  className="w-48 rounded-lg border border-black py-2 text-black transition hover:bg-gray-100 md:w-60"
+                  onClick={() => handleButtonClick("Standard")}
+                >
+                  Standard
+                </button>
+              </DialogTrigger>
+              <DialogTrigger asChild>
+                <button
+                  className="w-48 rounded-lg bg-primary py-2 text-white transition hover:bg-orange-600 md:w-60"
+                  onClick={() => handleButtonClick("Premium")}
+                >
+                  Premium
+                </button>
+              </DialogTrigger>
+              <DialogTrigger asChild>
+                <button
+                  className="w-48 rounded-lg bg-black py-2 text-white transition hover:bg-gray-800 md:w-60"
+                  onClick={() => handleButtonClick("Luxury")}
+                >
+                  Luxury
+                </button>
+              </DialogTrigger>
+            </div>
+
+            {/* Dialog Content */}
+            <DialogContent className="!md:py-16 max-w-2xl !rounded-2xl p-8 md:px-12">
+              <DialogHeader>
+                {/* Package Selection Buttons */}
+                <DialogTitle>
+                  <div className="flex w-full gap-2">
+                    {["Standard", "Premium", "Luxury"].map((pkg) => (
+                      <button
+                        key={pkg}
+                        onClick={() => setSelectedPackage(pkg)}
+                        className={`w-full rounded-lg px-4 py-2 text-sm text-white transition md:px-8 ${
+                          selectedPackage === pkg ? "bg-primary" : "bg-black"
+                        }`}
+                      >
+                        {pkg}
+                      </button>
+                    ))}
+                  </div>
+                </DialogTitle>
+
+                {/* Package Details */}
+                <DialogDescription>
+                  <div className="mt-6 flex w-full flex-col gap-3 sm:flex-row md:gap-12">
+                    {/* Inclusions */}
+                    <div className="w-full rounded-3xl border border-gray-100 p-8 shadow-2xl">
+                      <h3 className="font-poppins tracking-widest text-black md:text-lg md:font-light">
+                        Inclusions
+                      </h3>
+                      <ul className="ml-3 mt-4 list-disc font-poppins tracking-wider font-light text-gray-700 sm:mb-8">
+                        <li>Inclusion 1 for {selectedPackage}.</li>
+                        <li>Inclusion 2 for {selectedPackage}.</li>
+                        <li>Inclusion 3 for {selectedPackage}.</li>
+                      </ul>
+                    </div>
+
+                    {/* Exclusions */}
+                    <div className="w-full rounded-3xl border border-gray-100 p-8 shadow-2xl">
+                      <h3 className="font-poppins tracking-widest text-black md:text-lg md:font-light">
+                        Exclusions
+                      </h3>
+                      <ul className="ml-3 mt-4 list-disc font-poppins tracking-wider font-light text-gray-700 sm:mb-8">
+                        <li>Exclusion 1 for {selectedPackage}.</li>
+                        <li>Exclusion 2 for {selectedPackage}.</li>
+                        <li>Exclusion 3 for {selectedPackage}.</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="mt-8 flex items-center justify-center">
+                    <button className="w-48 rounded-lg bg-primary p-2 font-medium text-white">
+                      Get Quote
+                    </button>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+
+          <hr className="my-4" />
+          <button className="w-full rounded-lg bg-primary py-2 font-semibold text-white hover:bg-orange-500">
             Get Quote
           </button>
         </div>
@@ -119,7 +222,9 @@ export default function Departure({
           {maxAltInM && maxAltInM !== 0 && (
             <DepartureFact
               title="max altitude"
-              desc={`${maxAltInM.toLocaleString("en-us")}m/${(maxAltInM * 3.281).toLocaleString("en-us")}ft`}
+              desc={`${maxAltInM.toLocaleString(
+                "en-us",
+              )}m/${(maxAltInM * 3.281).toLocaleString("en-us")}ft`}
               icon={<FaMountain size={20} />}
             />
           )}
@@ -146,14 +251,13 @@ export default function Departure({
           className="z-10 mx-auto hidden w-fit gap-x-2 underline lg:flex"
         >
           <Flag size={16} />
-          <Text variant="text-sm" className="w-fit text-sm tracking-wide">
-            Report this listing
-          </Text>
+          <span className="text-sm tracking-wide">Report this listing</span>
         </Link>
       )}
     </div>
   );
 }
+
 export const DepartureFact = ({
   title,
   desc,
--- src/lib/server-axios-instance.ts
+++ src/lib/server-axios-instance.ts
@@ -11,7 +11,6 @@ export const axiosInstance = axios.create({
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
   },
 });
 
